df44f678e5edddbf291861aadfe492b6834f1ae5
iman-keangkasa <hafiz.ghazman@gmail.com>
2020-05-30 20:17:25 +0800

    Script for environment setup

---
+++ my_env.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+cp vimrc ~/.vimrc
+cp git-completion.bash ~/.git-completion.bash
+cp git-prompt.sh ~/.git-prompt.sh
+cp -r vim ~/.vim
+
+sudo apt-get install libevent-dev ncurses-dev build-essential bison pkg-config
+if [ -d ~/version-control ];
+then
+	git clone https//github.com/tmux/tmux.git ~/version-control/
+	git --git-dir=$HOME/version-control/tmux/.git --work-tree=$HOME/version-control/tmux checkout 3.0
+	sh ~/version-control/tmux/autogen.sh
+	~/version-control/configure && make
+	sudo make install
+else
+	mkdir ~/version-control/		
+	git clone https//github.com/tmux/tmux.git ~/version-control/
+	git --git-dir=$HOME/version-control/tmux/.git --work-tree=$HOME/version-control/tmux checkout 3.0
+	sh ~/version-control/tmux/autogen.sh
+	~/version-control/configure && make
+	sudo make install
+fi
+
+cp tmux.conf ~/.tmux.conf
+
+cp bash_alias_list ~/.bash_alias_list
+cp bashrc ~/.bashrc
+
