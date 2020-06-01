753825bd5abf19cbf957ae0e7f20936b267accc5
iman-keangkasa <hafiz.ghazman@gmail.com>
2020-06-01 13:55:11 +0800

    Download all necessary tools

--- my_env.bash
+++ my_env.bash
@@ -4,24 +4,32 @@ cp git-completion.bash ~/.git-completion.bash
 cp git-prompt.sh ~/.git-prompt.sh
 cp -r vim ~/.vim
 
-sudo apt-get install libevent-dev ncurses-dev build-essential bison pkg-config
-if [ -d ~/version-control ];
+sudo apt-get install vim git tree libevent-dev ncurses-dev build-essential bison pkg-config
+
+git config --global core.edit "vim"
+git config --global user.name "iman-keangkasa"
+git config --global user.email "hafiz.ghazman@gmail.com"
+
+if [ -d $HOME/version-control ]
 then
-	git clone https//github.com/tmux/tmux.git ~/version-control/
-	git --git-dir=$HOME/version-control/tmux/.git --work-tree=$HOME/version-control/tmux checkout 3.0
+	echo "[TRUE] Debug here"
+	git clone https://github.com/tmux/tmux.git ~/version-control
+	git --git-dir=$HOME/version-control/tmux/.git --work-tree=$HOME/version-control/tmux checkout 3.1
 	sh ~/version-control/tmux/autogen.sh
 	~/version-control/configure && make
 	sudo make install
 else
+	echo "[FALSE] Debug here"
 	mkdir ~/version-control/		
-	git clone https//github.com/tmux/tmux.git ~/version-control/
-	git --git-dir=$HOME/version-control/tmux/.git --work-tree=$HOME/version-control/tmux checkout 3.0
+	git clone https://github.com/tmux/tmux.git ~/version-control/
+	git --git-dir=$HOME/version-control/tmux/.git --work-tree=$HOME/version-control/tmux checkout 3.1
 	sh ~/version-control/tmux/autogen.sh
 	~/version-control/configure && make
 	sudo make install
+fi
 
 cp tmux.conf ~/.tmux.conf
 
-cp bash_alias_list ~/.bash_alias_list
+cp bashrc_alias_list ~/.bash_alias_list
 cp bashrc ~/.bashrc
 
